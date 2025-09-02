d98e7acde2ab137366a71122b316c148d20a0e44
jvtrades69-crypto <jvtrades69@gmail.com>
2025-09-02 15:17:32 +0800

    Update register-commands.js

--- register-commands.js
+++ register-commands.js
@@ -10,19 +10,29 @@ const commands = [
   new SlashCommandBuilder()
     .setName('signal')
     .setDescription('Post a new trade signal in this channel')
-    .addStringOption(o => o.setName('asset').setDescription('Asset ticker, e.g., BTC / ETH / SOL').setRequired(true))
-    .addStringOption(o => o.setName('side').setDescription('Long or Short').setRequired(true).addChoices(
-      { name: 'Long', value: 'LONG' },
-      { name: 'Short', value: 'SHORT' },
-    ))
-    .addStringOption(o => o.setName('entry').setDescription('Entry (number or range)').setRequired(true))
-    .addStringOption(o => o.setName('sl').setDescription('SL').setRequired(false))
-    .addStringOption(o => o.setName('tp1').setDescription('Target 1').setRequired(false))
-    .addStringOption(o => o.setName('tp2').setDescription('Target 2').setRequired(false))
-    .addStringOption(o => o.setName('tp3').setDescription('Target 3').setRequired(false))
-    .addStringOption(o => o.setName('timeframe').setDescription('Timeframe (e.g., 1H, 4H)').setRequired(false))
-    .addStringOption(o => o.setName('reason').setDescription('Short rationale (<= 1000 chars)').setRequired(false))
-    .addAttachmentOption(o => o.setName('image').setDescription('Chart image').setRequired(false))
+    .addStringOption(o =>
+      o.setName('asset').setDescription('Asset ticker, e.g., BTC / ETH / SOL').setRequired(true))
+    .addStringOption(o =>
+      o.setName('side').setDescription('Long or Short').setRequired(true).addChoices(
+        { name: 'Long', value: 'LONG' },
+        { name: 'Short', value: 'SHORT' },
+      ))
+    .addStringOption(o =>
+      o.setName('entry').setDescription('Entry (number or range)').setRequired(true))
+    .addStringOption(o =>
+      o.setName('sl').setDescription('SL').setRequired(false))
+    .addStringOption(o =>
+      o.setName('tp1').setDescription('Target 1').setRequired(false))
+    .addStringOption(o =>
+      o.setName('tp2').setDescription('Target 2').setRequired(false))
+    .addStringOption(o =>
+      o.setName('tp3').setDescription('Target 3').setRequired(false))
+    .addStringOption(o =>
+      o.setName('timeframe').setDescription('Timeframe (e.g., 1H, 4H)').setRequired(false))
+    .addStringOption(o =>
+      o.setName('reason').setDescription('Short rationale (<= 1000 chars)').setRequired(false))
+    .addAttachmentOption(o =>
+      o.setName('image').setDescription('Chart image').setRequired(false))
     .toJSON(),
 ];
 
